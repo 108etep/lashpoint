e36b8b133ab9373bcc79ff4be2a3b4b353990af7
Yuzhnii <70344381+108etep@users.noreply.github.com>
2023-03-28 17:01:05 +0500

    added fixes-header and debugs thx wudu

--- js/main.js
+++ js/main.js
@@ -41,38 +41,7 @@ $(document).ready(function() {
     $(".body").toggleClass('bg-bar');
     $(".header__menu").toggleClass('active');
     $(".menu__btn").toggleClass('active');
-
-    var darkBlock = $("#overlay").length;
-
-    if (darkBlock) {
-      $("#overlay, .close-burger, .logo__container").remove();
-    } else {
-      var docHeight = $(document).height();
-      $("body").append("<div id='overlay'></div>");
-      $("#overlay")
-        .height(docHeight)
-        .css({
-          'opacity': 0.4,
-          'position': 'absolute',
-          'top': 0,
-          'left': 0,
-          'background-color': 'black',
-          'width': '100%',
-          'z-index': 1
-        });
-
-      $('<div class="logo__container"><img class="logo" src="images/logo.svg" alt=""></div>').insertBefore('.header__menu').css({
-        'position': 'fixed',
-        'bottom': '-100px',
-        'left': '0',
-        'right': '0',
-        'z-index': '9999'
-      }).animate({
-        'bottom': '0'
-      }, 500);
-  
-      
-    }
+    $(".mobile__menu").toggleClass('active');
   });
 
   
